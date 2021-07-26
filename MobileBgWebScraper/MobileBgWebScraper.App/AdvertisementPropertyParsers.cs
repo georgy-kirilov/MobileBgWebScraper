77c5b051b1a16b7a1a63b4261582f5d0e96d167a
George <georgynk17@gmail.com>
2021-07-26 11:19:34 +0300

    Refactor advertisement property parsing logic

---
+++ MobileBgWebScraper/MobileBgWebScraper.App/AdvertisementPropertyParsers.cs
@@ -0,0 +1,79 @@
+﻿namespace MobileBgWebScraper.App
+{
+    using System;
+    using System.Globalization;
+
+    public static class AdvertisementPropertyParsers
+    {
+        public static void ParseManufacturingDate(string input, AdvertisementInputModel advertisement)
+        {
+            if (input == null)
+            {
+                return;
+            }
+
+            string rawDateInput = input.Replace("г.", string.Empty);
+            string[] rawDateInputArgs = rawDateInput.Split(" ");
+
+            int month = DateTime.ParseExact(rawDateInputArgs[0], "MMMM", new CultureInfo("bg-BG")).Month;
+            int year = int.Parse(rawDateInputArgs[1]);
+
+            advertisement.ManufacturingDate = new DateTime(year, month, 1);
+        }
+
+        public static void ParseKilometrage(string input, AdvertisementInputModel advertisement)
+        {
+            if (input == null)
+            {
+                return;
+            }
+
+            int kilometrage = int.Parse(input
+                                         .Replace(" ", string.Empty)
+                                         .ToLower()
+                                         .Replace("км", string.Empty));
+
+            advertisement.Kilometrage = kilometrage;
+        }
+
+        public static void ParseHorsePowers(string input, AdvertisementInputModel advertisement)
+        {
+            if (input == null)
+            {
+                return;
+            }
+
+            int horsePowers = int.Parse(input
+                                        .Replace(" ", string.Empty)
+                                        .ToLower()
+                                        .Replace("к.с.", string.Empty));
+
+            advertisement.HorsePowers = horsePowers;
+        }
+
+        public static void ParseEngineType(string input, AdvertisementInputModel advertisement)
+        {
+            advertisement.EngineType = input?.Trim();
+        }
+
+        public static void ParseTransmissionType(string input, AdvertisementInputModel advertisement)
+        {
+            advertisement.TransmissionType = input?.Trim();
+        }
+
+        public static void ParseBodyStyle(string input, AdvertisementInputModel advertisement)
+        {
+            advertisement.BodyStyle = input?.Trim();
+        }
+
+        public static void ParseColorName(string input, AdvertisementInputModel advertisement)
+        {
+            advertisement.ColorName = input?.Trim();
+        }
+
+        public static void ParseEuroStandard(string input, AdvertisementInputModel advertisement)
+        {
+            advertisement.EuroStandard = input?.Trim();
+        }
+    }
+}
--- MobileBgWebScraper/MobileBgWebScraper.App/Program.cs
+++ MobileBgWebScraper/MobileBgWebScraper.App/Program.cs
@@ -1,22 +1,18 @@
 ﻿namespace MobileBgWebScraper.App
 {
     using System;
-    using System.Collections.Generic;
-    using System.Globalization;
     using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
+    using System.Collections.Generic;
+
     using AngleSharp;
-    using MobileBgWebScraper.Models;
+
+    using static AdvertisementPropertyParsers;
 
     public class Program
     {
         public static async Task Main()
         {
-            Console.OutputEncoding = Encoding.UTF8;
-
-            var advertisements = new List<AdvertisementInputModel>();
-
             var config = Configuration.Default.WithDefaultLoader();
             var context = BrowsingContext.New(config);
             var address = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=kvo3k4&f1=";
@@ -25,29 +21,30 @@
             var propertiesParsingTable = new Dictionary<string, Action<string, AdvertisementInputModel>>()
             {
                 { "дата на производство", ParseManufacturingDate },
-                { "тип двигател", (input, advert) => advert.EngineType = input?.Trim() },
+                { "тип двигател", ParseEngineType },
                 { "мощност", ParseHorsePowers },
-                { "скоростна кутия", (input, advert) => advert.TransmissionType = input?.Trim() },
-                { "категория", (input, advert) => advert.BodyStyle = input?.Trim() },
+                { "скоростна кутия", ParseTransmissionType },
+                { "категория", ParseBodyStyle },
                 { "пробег", ParseKilometrage },
-                { "цвят", (input, advert) => advert.ColorName = input?.Trim() },
-                { "евростандарт", (input, advert) => advert.EuroStandard = input?.Trim() },
+                { "цвят", ParseColorName },
+                { "евростандарт", ParseEuroStandard },
             };
 
-            for (int page = 1; page <= 150; page++)
+            for (int page = 1; page <= 10; page++)
             {
                 var document = await context.OpenAsync($"{address}{page}");
                 var urls = document.QuerySelectorAll(query).Select(a => a.GetAttribute("href").Trim());
 
-                foreach (var url in urls)
+                foreach (string url in urls)
                 {
                     var input = new AdvertisementInputModel();
                     var advertisementDocument = await context.OpenAsync($"https:{url}");
 
                     input.Title = advertisementDocument.QuerySelector("h1").TextContent;
 
-                    var href = advertisementDocument.QuerySelector("a.fastLinks").GetAttribute("href").Trim();
+                    string href = advertisementDocument.QuerySelector("a.fastLinks").GetAttribute("href").Trim();
                     var hrefArgs = href.Split("?")[1].Split("&");
+
                     input.BrandName = hrefArgs[1].Split("=")[1];
                     input.ModelName = hrefArgs[2].Split("=")[1];
 
@@ -92,59 +89,16 @@
                                                         .QuerySelector("tbody > tr > td")
                                                         .TextContent;
 
-                    string fullAddress = advertisementDocument.QuerySelectorAll("div.adress")[1].TextContent;
+                    var addressBlocks = advertisementDocument.QuerySelectorAll("div.adress");
+                    int addressBlockIndex = addressBlocks.Length > 1 ? 1 : 0;
+                    string fullAddress = addressBlocks[addressBlockIndex].TextContent.Trim();
+
                     var fullAddressArgs = fullAddress.Split(", ");
 
                     input.RegionName = fullAddressArgs[0];
                     input.TownName = fullAddressArgs[1];
-
-                    advertisements.Add(input);
                 }
             }
-
-            Console.WriteLine(advertisements.Count);
-        }
-
-        public static void ParseManufacturingDate(string input, AdvertisementInputModel advertisement)
-        {
-            if (input == null)
-            {
-                return;
-            }
-
-            string rawDateInput = input.Replace("г.", string.Empty);
-            string[] rawDateInputArgs = rawDateInput.Split(" ");
-
-            int month = DateTime.ParseExact(rawDateInputArgs[0], "MMMM", new CultureInfo("bg-BG")).Month;
-            int year = int.Parse(rawDateInputArgs[1]);
-
-            advertisement.ManufacturingDate = new DateTime(year, month, 1);
-        }
-
-        public static void ParseHorsePowers(string input, AdvertisementInputModel advertisement)
-        {
-            if (input == null)
-            {
-                return;
-            }
-
-            int horsePowers = int.Parse(input
-                                        .Replace(" ", string.Empty)
-                                        .ToLower()
-                                        .Replace("к.с.", string.Empty));
-
-            advertisement.HorsePowers = horsePowers;
-        }
-
-        public static void ParseKilometrage(string input, AdvertisementInputModel advertisement)
-        {
-            if (input == null)
-            {
-                return;
-            }
-
-            int kilometrage = int.Parse(input.Replace(" ", string.Empty).ToLower().Replace("км", string.Empty));
-            advertisement.Kilometrage = kilometrage;
         }
     }
 }
